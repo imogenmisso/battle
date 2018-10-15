f9cffe32d4567337559c7e0dd15ab82d0bc52080
Henry Cooksley <henry.cooksley@gmail.com>
2018-10-15 15:25:08 +0100

    add name to cat

--- app.rb
+++ app.rb
@@ -1,21 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  "<h1>hello!</h1>"
-end
 
-get '/secret' do
-  "I neither love nor hate marmite"
-end
-
-get '/greeting' do
-  "hi imogen. you rock."
-end
-
-get '/joke' do
-  "Why did the coffee go to the police. ... Because it got mugged."
 end
 
 get '/cat' do
-  "<img src='http://bit.ly/1eze8aE' style='border: dashed red;'>"
+  erb(:index)
 end
